f604bc0b12194b8bd8c3dd51747ea682535283ea
Igor Bykowski <igby@stibosystems.com>
2024-09-22 13:25:59 +0200

    Add transloco

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,24 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 import {appRoutes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
+import {provideHttpClient} from '@angular/common/http';
+import {TranslocoHttpLoader} from './transloco-loader';
+import {provideTransloco} from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideHttpClient(), provideTransloco({
+        config: { 
+          availableLangs: ['en', 'pl', 'de'],
+          defaultLang: 'en',
+          // Remove this option if your application doesn't support changing language in runtime.
+          reRenderOnLangChange: true,
+          prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+      }),
   ],
 };
